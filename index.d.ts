eaa452ecfa419bd20af38a577c74f32238b85245
kundanbhosale <kundanmbhosale@gmail.com>
2024-01-04 12:24:47 +0530

    Added Shared Types

--- index.d.ts
+++ index.d.ts
@@ -5,3 +5,9 @@ declare module "knex/types/tables" {
   // eslint-disable-next-line @typescript-eslint/no-empty-interface
   export interface Tables extends DBTables {}
 }
+
+declare module "lipy-ai" {
+  export interface Test {
+    id: string
+  }
+}
