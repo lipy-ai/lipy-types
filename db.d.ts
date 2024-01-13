f9123c811461c9146bbbe238a93d129d046bd4f3
utsavchab <21je1011@iitism.ac.in>
2024-01-13 22:30:39 +0530

    invitationTable correction

--- db.d.ts
+++ db.d.ts
@@ -29,7 +29,7 @@ export interface OrgInvitationTable {
   org_id: OrgListTable['id']
   user_id: UserListTable['id']
   invited_by: UserListTable['id']
-  message_id: string | null
+  message_id?: string
   status: InvitationStatus
   type: UserType
   invited_at: string | Date
